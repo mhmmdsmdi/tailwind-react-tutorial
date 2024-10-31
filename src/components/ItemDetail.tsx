9575c6f465c84a7c914198ef945804ad4ab9779f
Mohammad Samadi <m.samadi1377@gmail.com>
2024-10-31 17:09:18 +0330

    feat: make item detail responsive

--- src/components/ItemDetail.tsx
+++ src/components/ItemDetail.tsx
@@ -4,8 +4,8 @@ function ItemDetail() {
   return (
     <div className="flex flex-col lg:flex-row-reverse space-y-4">
       {/* Item Image */}
-      <div className="flex-1">
-        <div className="bg-gradient-to-br from-[#F637CF] from-5% via-[#E3D876] via-40% to-[#4DD4C6]">
+      <div className="flex-1 lg:-mt-32 lg:ml-28">
+        <div className="flex-center h-full bg-gradient-to-br from-[#F637CF] from-5% via-[#E3D876] via-40% to-[#4DD4C6]">
           <img src="/assets/n1-min.png" />
         </div>
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
   const [isMobileMenuShow, setIsMobileMenuShow] = useState(false);
 
   return (
-    <nav className="flex flex-wrap justify-between items-center">
+    <nav className="flex flex-wrap justify-between items-center z-10 relative">
       {/* Logo */}
       <a href="#">
         <MdHomeRepairService size={45} />
@@ -27,10 +27,22 @@ function Nav() {
       <div
         className={`${!isMobileMenuShow && "hidden"} w-full lg:w-auto lg:block pt-2`}
       >
-        <ul className="flex flex-col lg:space-x-8 lg:flex-row lg:bg-transparent lg:border-none text-lg border bg-gray-50 border-gray-100 rounded p-4">
+        <ul
+          className="flex flex-col lg:space-x-8 
+        lg:flex-row lg:bg-transparent
+         lg:border-none text-lg border 
+         bg-gray-50 border-gray-100 rounded p-4"
+        >
           {routes.map((route, index) => (
             <li
-              className={`px-3 py-2 rounded-md  cursor-pointer ${index == 0 ? "bg-blue-500 lg:bg-transparent lg:text-blue-500 text-white" : "hover:bg-gray-100"}`}
+              className={`px-3 py-2
+                 rounded-md
+                 cursor-pointer
+                 lg:hover:bg-transparent 
+                lg:hover:text-blue-500
+                ${index == 0 ? "bg-blue-500 lg:bg-transparent lg:text-blue-500 text-white" : "hover:bg-gray-100"}
+                ${(index === 3 || index === 4) && "lg:text-white"}
+                `}
               key={route}
             >
               <a href="#">{route}</a>
@@ -40,8 +52,8 @@ function Nav() {
       </div>
 
       {/* Cart button */}
-      <div className="fixed left-4 bottom-4 lg:static">
-        <div className="flex-center rounded-full h-12 w-12 bg-white shadow-md ">
+      <div className="fixed left-4 bottom-4 lg:static lg:mr-8">
+        <div className="flex-center rounded-full h-12 w-12 cursor-pointer bg-white shadow-md ">
           <RiShoppingBag2Fill />
         </div>
       </div>
